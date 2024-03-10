28329bf3932208deba2ebdf16c2a5c09107eb25d
Vimer38 <andkir205@yandex.ru>
2024-03-10 17:07:03 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Button/Header/Header";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
-import {Route, Routes} from "react-router-dom";
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, []);
+    }, [])
 
     return (
         <div className="App">
