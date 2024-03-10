e24cbfa6159fbb5f7c4ee43c8937138439838d04
Vimer38 <andkir205@yandex.ru>
2024-03-10 16:26:40 +0300

    init

--- src/components/Button/Header/Header.jsx
+++ src/components/Button/Header/Header.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Button from "../Button";
+import {useTelegram} from "../../../hooks/useTelegram";
+import './Header.css';
 const Header = () =>{
-    const tg = window.Telegram.WebApp;
-
-    const onClose = () => {
-        tg.close()
-    }
+    const {user, onCLose} = useTelegram();
 
     return (
-        <div>
+        <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
-    )
-}
+    );
+};
 export default Header;
